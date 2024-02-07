8ebded598a58c58d7e73ee7ae9494702d686e3f0
Shreyansh Gupta <40631571+shreyansh-g24@users.noreply.github.com>
2024-02-07 14:36:54 +0530

    Sets up grocery to be removed from the cart if it is marked as out of stock.

--- app/models/Grocery.rb
+++ app/models/Grocery.rb
@@ -4,4 +4,12 @@ class Grocery < ApplicationRecord
 
   validates_presence_of :name
   validates :price_per_unit, numericality: { greater_than: 0 }
+
+  before_save :remove_groceries_orders, if: :is_out_of_stock?
+
+  private
+
+  def remove_groceries_orders
+    groceries_orders.joins(:order).where({ order: { status: "in_cart" } }).destroy_all
+  end
 end
